7af5c1d9961e15cf97ba64c448709bdba96d6077
muhamadtegar1 <tegaradyaksa03@gmail.com>
2024-05-15 15:35:47 +0800

    a

--- app/src/main/java/projek/akhir/klp7/App.java
+++ app/src/main/java/projek/akhir/klp7/App.java
@@ -5,7 +5,7 @@ package projek.akhir.klp7;
 
 public class App {
     public String getGreeting() {
-        return "Hello GLOBE";
+        return "Hello World";
     }
 
     public static void main(String[] args) {
